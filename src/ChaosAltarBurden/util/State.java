d18c15d74d4f8c80aed3c8d1848a8d089c541d87
MarcoGDev <68064902+MarcoGDev@users.noreply.github.com>
2023-02-24 19:29:16 +0100

    Update State.java
    Cleaning up whitespace

--- src/ChaosAltarBurden/util/State.java
+++ src/ChaosAltarBurden/util/State.java
@@ -2,16 +2,9 @@ package ChaosAltarBurden.util;
 
 public class State {
 
-
-
     public static boolean worldIsEmpty = false;
-
 
     public static void setEmpty(boolean empty) {
         worldIsEmpty = empty;
     }
-
-
-
 }
-
